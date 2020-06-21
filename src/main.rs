4c6c8824226f4ccc7fcd53b1229329ea48a0ad97
Emanuel Machado da Silva <emanuel.mch@gmail.com>
2020-06-21 20:10:22 -0300

    Introduce offspring

--- src/main.rs
+++ src/main.rs
@@ -25,12 +25,16 @@ use rand::Rng;
 use std::mem::MaybeUninit;
 
 const POPULATION_SIZE: usize = 40;
+const CHROMOSOME_SIZE: usize = 10;
+const MAX_GENE: i8 = CHROMOSOME_SIZE as i8;
 const MAX_FITNESS: u8 = 100;
+
 const MAX_ITERATIONS: u32 = 5000;
+const ELITE: usize = 6;
 
 #[derive(Copy, Clone)]
 struct Chromosome {
-    genes: [i8; 10],
+    genes: [i8; CHROMOSOME_SIZE],
     fitness: u8,
 }
 
@@ -38,18 +42,18 @@ impl Default for Chromosome {
     fn default() -> Self {
         let mut rng = rand::thread_rng();
 
-        let mut genes = [0; 10];
-        for i in 0..10 {
-            genes[i] = rng.gen_range(0, 10);
+        let mut genes = [0; CHROMOSOME_SIZE];
+        for i in 0..CHROMOSOME_SIZE {
+            genes[i] = rng.gen_range(0, MAX_GENE);
         }
         let fitness = fitness(genes);
         return Chromosome { genes, fitness };
     }
 }
 
-fn fitness(genes: [i8; 10]) -> u8 {
+fn fitness(genes: [i8; CHROMOSOME_SIZE]) -> u8 {
     let mut result = 0;
-    for i in 0..10 {
+    for i in 0..genes.len() {
         // TODO Can I remove this cast somehow?
         if genes[i] == i as i8 {
             result += 1;
@@ -58,43 +62,73 @@ fn fitness(genes: [i8; 10]) -> u8 {
     return result;
 }
 
+// Assumes population is sorted
 fn find_best(population: [Chromosome; POPULATION_SIZE]) -> Chromosome {
-    let mut sorted = population.clone();
-    sorted.sort_by(|a, b| a.fitness.cmp(&b.fitness));
-    return sorted.last().expect("Population was empty, somehow").to_owned();
+    return population.last().expect("Population was empty, somehow").to_owned();
 }
 
+// Assumes population is sorted, returns an unsorted one
 fn next_generation(population: [Chromosome; POPULATION_SIZE]) -> [Chromosome; POPULATION_SIZE] {
-    let next = unsafe {
-        let mut arr: [Chromosome; POPULATION_SIZE] = MaybeUninit::uninit().assume_init();
-        for i in 0..arr.len() {
-            arr[i] = Default::default();
-        }
-        arr
-    };
-    return next;
+    const START: usize = POPULATION_SIZE - ELITE - 1;
+    const END: usize = POPULATION_SIZE - 1;
+    let elite = &population[START..END];
+    assert_eq!(elite.len(), ELITE);
+
+    let mut offspring = unsafe { MaybeUninit::<[Chromosome; ELITE]>::uninit().assume_init() };
+    for i in (0..elite.len()).step_by(2) {
+        let mom = elite[i];
+        let dad = elite[i + 1];
+
+        const MIDDLE: usize = CHROMOSOME_SIZE / 2;
+
+        let mut first = [0; CHROMOSOME_SIZE];
+        mom.genes[0..MIDDLE].iter().enumerate().for_each(|(i, gene)| first[i] = gene.to_owned());
+        dad.genes[MIDDLE..CHROMOSOME_SIZE].iter().enumerate().for_each(|(i, gene)| first[i + MIDDLE] = gene.to_owned());
+        let first_fitness = fitness(first);
+        offspring[i] = Chromosome { genes: first, fitness: first_fitness };
+
+        let mut second = [0; CHROMOSOME_SIZE];
+        dad.genes[0..MIDDLE].iter().enumerate().for_each(|(i, gene)| second[i] = gene.to_owned());
+        mom.genes[MIDDLE..CHROMOSOME_SIZE].iter().enumerate().for_each(|(i, gene)| second[i + MIDDLE] = gene.to_owned());
+        let second_fitness = fitness(second);
+        offspring[i + 1] = Chromosome { genes: second, fitness: second_fitness };
+    }
+
+    assert_eq!(offspring.len(), ELITE);
+
+    let mut final_population = unsafe { MaybeUninit::<[Chromosome; POPULATION_SIZE]>::uninit().assume_init() };
+
+    for (i, chromosome) in elite.iter().enumerate() { final_population[i] = chromosome.to_owned() }
+    for (i, chromosome) in offspring.iter().enumerate() { final_population[i + ELITE] = chromosome.to_owned() };
+    for i in (ELITE + offspring.len())..POPULATION_SIZE { final_population[i] = Default::default(); }
+
+    return final_population;
 }
 
 fn run() -> Chromosome {
-    let mut population = unsafe {
-        let mut arr: [Chromosome; POPULATION_SIZE] = MaybeUninit::uninit().assume_init();
-        for i in 0..arr.len() {
-            arr[i] = Default::default();
-        }
-        arr
-    };
+    let mut population = unsafe { MaybeUninit::<[Chromosome; POPULATION_SIZE]>::uninit().assume_init() };
+
+    for i in 0..population.len() {
+        population[i] = Default::default();
+    }
 
     for _ in 0..MAX_ITERATIONS {
+        // TODO Create a Population struct instead of assuming it's always sorted in this step
+        population.sort_by(|a, b| a.fitness.cmp(&b.fitness));
+
         let best_individual = find_best(population);
         if best_individual.fitness == MAX_FITNESS { return best_individual; }
 
         population = next_generation(population);
     }
 
+    population.sort_by(|a, b| a.fitness.cmp(&b.fitness));
     return find_best(population);
 }
 
 fn main() {
+    assert_eq!((ELITE % 2), 0);
+    assert!((ELITE * 2) < POPULATION_SIZE);
     let result = run();
     println!("Best candidate has fitness {}: {:?}", result.fitness, result.genes);
 }
